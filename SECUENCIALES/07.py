e2c545510e2f9efab291c308adb1e3070c96e044
LEOLUCAS <gloriaxd77@hotmail.com>
2023-02-16 10:25:09 -0500

    07

---
+++ SECUENCIALES/07.py
@@ -0,0 +1,10 @@
+
+BaseRectangul = int(input("INGRESE BASE: "))
+AlturaRectangul = int(input("INGRESE ALTURA: "))
+
+Area = BaseRectangul*AlturaRectangul
+Perimetro= 2*(BaseRectangul+AlturaRectangul)
+print()
+print(f"Area={format(Area,'.2f')} u2 ")
+print(f"Perimetro={format(Perimetro,'.2f')} u ")
+
