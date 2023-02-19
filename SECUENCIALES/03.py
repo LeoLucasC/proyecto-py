cab6b715d45f6c4a24f2d3fae36f21002cc4868b
LEOLUCAS <gloriaxd77@hotmail.com>
2023-02-19 12:09:56 -0500

    03

--- SECUENCIALES/03.py
+++ SECUENCIALES/03.py
@@ -1,2 +1,18 @@
+import os
+os.system("cls")
+
+kilometros=int(input("kilometros: "))
+pies=int(input("pies: "))
+millas=int(input("millas: "))
 
 
+metro=kilometros*1000
+pies = metro/ 3280.84
+millas=metro/1609.34
+
+
+
+
+print(f"el primer tramo en metro es: {format(metro,' .2f')} ")
+print(f"el segundo  tramo en pies     es:{format(metro,' .2f')}")
+print(f"el tercer tramo en millas     es:{format(millas,' .2f')} ")
