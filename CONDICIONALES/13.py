b441bcce75bc0a53f2d0f2a8756dbc39733fa991
LEOLUCAS <gloriaxd77@hotmail.com>
2023-02-26 12:33:30 -0500

    04

--- CONDICIONALES/13.py
+++ CONDICIONALES/13.py
@@ -1,9 +1,14 @@
 import os 
 os.system("cls")
 
-numero=int(input("NUMERO: "))
+c1=int(input("CIFRA 1: "))
+c2=int(input("CIFRA 2: "))
+c3=int(input("CIFRA 3: "))
 
-if numer
+if c1>c2 and c2>c3 : tipo="NUMERO DE CIFRAS ASCENDENTE"
+elif c1<c2 and c2<c3 : tipo="NUMERO DE CIFRAS DESCENDENTE"
+else : tipo="error"
+print(tipo)
 
 
 
